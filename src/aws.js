9051eacbee1ba107794df0d49e30d80a0b3120ae
Dkarki <contactdipak@gmail.com>
2024-02-05 18:32:26 +0000

    Removed custom bootstrap script for windows

--- src/aws.js
+++ src/aws.js
@@ -40,8 +40,6 @@ function buildUserDataScriptForWindows(githubRegistrationToken, label) {
     return [
       '<powershell>',
       `cd "${config.input.runnerHomeDir}"`,
-      `echo "${config.input.preRunnerScript}" > pre-runner-script.bat`,
-      '& pre-runner-script.bat',
       `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --name ${label} --unattended`,
       './run.cmd',
       '</powershell>',
@@ -51,8 +49,6 @@ function buildUserDataScriptForWindows(githubRegistrationToken, label) {
     return [
       '<powershell>',
       `mkdir actions-runner; cd actions-runner`,
-      `echo "${config.input.preRunnerScript}" > pre-runner-script.bat`,
-      '& pre-runner-script.bat',
       `Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v${runnerVersion}/actions-runner-win-x64-${runnerVersion}.zip -OutFile actions-runner-win-x64-${runnerVersion}.zip`,
       `Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-${runnerVersion}.zip", "$PWD")`,
       `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --name ${label} --unattended`,
